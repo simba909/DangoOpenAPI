80bf9ed2a5b229d0b77f385a4506de1e722c04f9
Simon Jarbrant <simonjarbrant@me.com>
2024-01-19 13:23:10 +0100

    Add explicit dependency on HTTPTypes to Data
    Without this, building for testing inside of Xcode fails with linker errors.

--- Modules/Data/Package.swift
+++ Modules/Data/Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         .library(name: "DataMocks", targets: ["DataMocks"])
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-http-types", .upToNextMinor(from: "1.0.3")),
         .package(url: "https://github.com/apple/swift-openapi-generator", .upToNextMinor(from: "1.2.0")),
         .package(url: "https://github.com/apple/swift-openapi-runtime", .upToNextMinor(from: "1.2.0")),
         .package(url: "https://github.com/apple/swift-openapi-urlsession", .upToNextMinor(from: "1.0.0")),
@@ -20,6 +21,8 @@ let package = Package(
         .target(
             name: "Data",
             dependencies: [
+                .product(name: "HTTPTypes", package: "swift-http-types"),
+                .product(name: "HTTPTypesFoundation", package: "swift-http-types"),
                 .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
                 .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
             ],
