3d6acc9bb6933a2168bf39c5344bcc006371e3ad
Simon Jarbrant <simonjarbrant@me.com>
2024-01-19 11:50:21 +0100

    Add SwiftLint plugin to app target

---
+++ Modules/SwiftLintPlugin/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftLintPlugin",
+    products: [
+        .plugin(name: "SwiftLint", targets: ["SwiftLint"]),
+    ],
+    targets: [
+        .plugin(
+            name: "SwiftLint",
+            capability: .buildTool(),
+            dependencies: ["SwiftLintBinary"]
+        ),
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.54.0/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "963121d6babf2bf5fd66a21ac9297e86d855cbc9d28322790646b88dceca00f1"
+        ),
+    ]
+)
