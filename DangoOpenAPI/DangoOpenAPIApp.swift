2c4f3784d8f0ad84f8e065dd6cc029140748f517
Simon Jarbrant <simonjarbrant@me.com>
2024-01-19 10:35:19 +0100

    Initial Commit

---
+++ DangoOpenAPI/DangoOpenAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DangoOpenAPIApp.swift
+//  DangoOpenAPI
+//
+//  Created by Simon Jarbrant on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct DangoOpenAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
