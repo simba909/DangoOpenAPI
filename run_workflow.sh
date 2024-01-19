3ed2386bd9c569f270d8592c7586079ac15d06da
Simon Jarbrant <simonjarbrant@me.com>
2024-01-19 11:23:37 +0100

    Add workflow runner script

---
+++ run_workflow.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Clean up any previous run
+rm -rf workspace
+xcodebuild clean -scheme DangoOpenAPI -project DangoOpenAPI.xcodeproj
+
+# Create necessary directory structure and files
+mkdir -p workspace/tmp
+touch workspace/tmp/resultBundleStream.json
+
+# Build for testing
+xcodebuild build-for-testing \
+    -scheme DangoOpenAPI \
+    -project DangoOpenAPI.xcodeproj \
+    -destination "platform=iOS Simulator" \
+    -testProductsPath workspace/TestProducts.xctestproducts \
+    -derivedDataPath workspace/DerivedData \
+    -resultBundleVersion 3 \
+    -resultBundlePath workspace/resultbundle.xcresult \
+    -resultStreamPath workspace/tmp/resultBundleStream.json  \
+    -IDEPostProgressNotifications=YES \
+    COMPILER_INDEX_STORE_ENABLE=NO \
+    -hideShellScriptEnvironment
